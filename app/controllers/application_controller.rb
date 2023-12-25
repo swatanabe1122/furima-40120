d68c1dfaa0ab36839bc90cea7486d2805a2e9667
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2023-12-25 15:26:54 +0900

    ユーザー管理機能
    ログインの有無で表示が変わるように分岐、　　　　　　　　　　　　　　　　　　　　　CSSファイル追加してなかったので追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   private
 
   def basic_auth
@@ -14,8 +15,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :nickname, :email, :password, :password_confirmation,
-      :last_name, :first_name, :last_name_kana, :first_name_kana,
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana,
       :birthday
     ])
   end
