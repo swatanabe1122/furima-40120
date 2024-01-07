800e4f01ec8a30e72fe4aec1dca5e425395af1a5
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2024-01-07 17:17:11 +0900

    エラーハンドリング画面の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_status
   belongs_to :shipping_cost
-  # ユーザーの関連付け
   belongs_to :user
-  #商品の画像を投稿
   has_one_attached :image
-  # 注文の関連付け
   has_one :order
 
-  validates :item_status_id, numericality: { other_than: 0 } 
-  # コメントの関連付け
-  #has_many :comments
-  
   validates :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, presence: true
   validates :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 0, message: "must be selected" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
