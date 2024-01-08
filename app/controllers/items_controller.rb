57ff46eda1e7dccd0b8615c64512ad268a3cc1c0
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2024-01-08 16:41:23 +0900

    destroyアクションのルーティングを設定
    削除ボタンも設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-     @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -15,14 +15,13 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path 
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    
   end
 
   def edit
@@ -39,17 +38,22 @@ class ItemsController < ApplicationController
       render 'edit', status: :unprocessable_entity
     end
   end
-  
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
   def item_params
     # ストロングパラメータを設定
-    params.require(:item).permit(:name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
-   end
+  end
 end
