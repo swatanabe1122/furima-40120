6f40b7dbb30344ee24acfe42088f996395fdc276
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2024-01-08 13:14:47 +0900

    ログイン状態の場合かどうかで表示を変更できるように実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,12 +23,12 @@ class ItemsController < ApplicationController
 
   private
 
-   #def set_item
-    #@item = Item.find(params[:id])
-  #end
-
   def item_params
     # ストロングパラメータを設定
     params.require(:item).permit(:name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
+  
+  def set_item
+    @item = Item.find(params[:id])
+   end
 end
