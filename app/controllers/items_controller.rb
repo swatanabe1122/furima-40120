1f327b3da33d744ee959e960ee46d5d555d90a27
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2024-01-08 16:52:34 +0900

    findメソッドが複数箇所で使用されている為修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   # ユーザーがログインしていなければ、そのユーザーをログイン画面に遷移させる
   before_action :authenticate_user!, except: [:index, :show]
-
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -40,7 +39,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
   end
