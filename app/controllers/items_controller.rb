69cad6c88dff64f30e140b7914eda9385191a2af
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2024-01-08 14:10:18 +0900

    showアクションの定義の記述を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,13 +21,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     # ストロングパラメータを設定
     params.require(:item).permit(:name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
    end
